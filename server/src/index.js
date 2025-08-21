c513505d267a4dd28240036336990a031ce787d0
Jay Vallespin <jayvallespin51@gmail.com>
2025-08-21 14:44:01 +0800

    Add new origin

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-management-system-client.onrender.com",
+    ],
     credentials: true,
   })
 );
